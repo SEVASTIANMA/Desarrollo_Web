c60d6cfa59e1feb321c4c37d0bfa94c31c0488df
Jarid Osorio <jariddavid78@gmail.com>
2026-09-24 10:38:18 -0500

    ejercicio python
    se implemento el ejercicio de 5 notas y promedio en python

---
+++ JaridOsorio/01_Ejercicios_Diarios/Lenguaje_Programacion/Promedio_de_notas/app.py
@@ -0,0 +1,15 @@
+
+nota1 = float(input("Ingresa la nota 1: "))
+nota2 = float(input("Ingresa la nota 2: "))
+nota3 = float(input("Ingresa la nota 3: "))
+nota4 = float(input("Ingresa la nota 4: "))
+nota5 = float(input("Ingresa la nota 5: "))
+
+suma_notas = nota1 + nota2 + nota3 + nota4 + nota5
+
+promedio = suma_notas / 5
+
+print()
+print("-" * 40)
+print(f"El promedio final de las 5 notas es: {promedio}")
+print("-" * 40)
